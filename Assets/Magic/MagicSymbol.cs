7aee6c08c24b7488fad4e214992dd390f763aefb
uberblah <miles.rufatlatre@colorado.edu>
2015-01-27 13:26:19 -0700

    init commit

---
+++ Assets/Magic/MagicSymbol.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicSymbol : MonoBehaviour
+{
+    Texture2D glyph;
+    MagicVM.Operation op;
+}
---
+++ Assets/Magic/MagicVM.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicVM : MonoBehaviour
+{
+    public delegate void Operation(MagicVM vm);
+
+    protected Stack stack;
+    protected MagicScroll scroll;
+    protected MagicScroll.Enumerator ip;
+
+    public void SetScroll(MagicScroll newScroll)
+    {
+        scroll = newScroll;
+        ip = scroll.GetEnumerator();
+    }
+
+    protected virtual void Update()
+    {
+        //TODO: execute a single instruction
+    }
+}
