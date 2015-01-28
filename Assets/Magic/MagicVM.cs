22bd1bbfe9048f4cee84ad0b6e293f3c1b88b0a0
uberblah <miles.rufatlatre@colorado.edu>
2015-01-28 11:08:57 -0700

    added failure condition for MagicVM

--- Assets/Magic/MagicVM.cs
+++ Assets/Magic/MagicVM.cs
@@ -5,9 +5,15 @@ public class MagicVM : MonoBehaviour
 {
     public delegate void Operation(MagicVM vm);
 
-    protected Stack stack;
-    protected MagicScroll scroll;
+    protected Stack stack = new Stack();
+    protected MagicScroll scroll = null;
     protected MagicScroll.Enumerator ip;
+    public Operation onFailure = DefaultFailure;
+
+    static void DefaultFailure(MagicVM vm)
+    {
+        Debug.Log(vm.Pop());
+    }
 
     public void SetScroll(MagicScroll newScroll)
     {
@@ -28,6 +34,17 @@ public class MagicVM : MonoBehaviour
     public void Push(object o)
     {
         stack.Push(o);
+    }
+
+    public void Execute()
+    {
+        object o = stack.Pop();
+        Operation op = o as Operation;
+        if (op == null)
+        {
+            stack.Push("EXECUTE:NOT_AN_OP(" + o.ToString() + ")");
+            onFailure(this);
+        }
     }
 
     void Update()
