c8977eaea9b6986bc5becef6419f184d2b3b1b64
Develra <michael.aaron@colorado.edu>
2015-04-19 22:08:07 -0600

    Add ITween

--- Assets/Player/Scripts/PlayerDamage.cs
+++ Assets/Player/Scripts/PlayerDamage.cs
@@ -5,25 +5,43 @@ public class PlayerDamage : MonoBehaviour {
 
 	public int currentHealth;
 	public int invinsibleTimer;
-	// Use this for initialization
-	void Start () {
-	}
 
 	void FixedUpdate() {
 		//decrement our invinsibility timer if our player is invinsible
 		if (invinsibleTimer != 0)
 			invinsibleTimer -= 1;
+		//check for death
+		if (currentHealth == 0)
+			Death ();
 	}
 	void OnCollisionStay2D (Collision2D other){
-		if (invinsibleTimer == 0) {
+		if (invinsibleTimer <= 0) {
 			if (other.gameObject.tag == "Low Damage"){
 				currentHealth -= 1;
 				invinsibleTimer = 50;
 			}
+			else if (other.gameObject.tag == "Medium Damage"){
+				currentHealth -= 2;
+				invinsibleTimer = 50;
+			}
+			else if (other.gameObject.tag == "High Damage"){
+				currentHealth -= 3;
+				invinsibleTimer = 50;
+			}
+			else if (other.gameObject.tag == "Instakill"){
+				currentHealth = 0;
+				invinsibleTimer = 50;
+			}
+
 		}
 	}
 	//Health Getter
 	public int GetHealth(){
 		return currentHealth;
+	}
+
+	private void Death(){
+		//we should do something fancy here, but i'm just going to reload the level for now
+		Application.LoadLevel(Application.loadedLevelName);
 	}
 }
