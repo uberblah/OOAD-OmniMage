8c399e97c22ca574c57ec4f6ef5518b911af94dd
Develra <michael.aaron@colorado.edu>
2015-04-19 19:16:17 -0600

    added basic player HUD with hearts

---
+++ Assets/UI/HUD/Scripts/HeartRender.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartRender : MonoBehaviour {
+	public int currentHealth = 6;
+
+	public Sprite fullHeart;
+	public Sprite halfHeart;
+	public Sprite emptyHeart;
+
+	public GameObject heartOne;
+	public GameObject heartTwo;
+	public GameObject heartThree;
+	// Update is called once per frame
+	void Update () {
+		switch (currentHealth){
+			case 6:
+				heartOne.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				heartTwo.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				heartThree.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				break;
+			case 5:
+				heartOne.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				heartTwo.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				heartThree.GetComponent<SpriteRenderer>().sprite = halfHeart;
+				break;
+			case 4:
+				heartOne.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				heartTwo.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				heartThree.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				break;
+			case 3:
+				heartOne.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				heartTwo.GetComponent<SpriteRenderer>().sprite = halfHeart;
+				heartThree.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				break;
+			case 2:
+				heartOne.GetComponent<SpriteRenderer>().sprite = fullHeart;
+				heartTwo.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				heartThree.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				break;
+			case 1:
+				heartOne.GetComponent<SpriteRenderer>().sprite = halfHeart;
+				heartTwo.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				heartThree.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				break;
+			default:
+				heartOne.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				heartTwo.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				heartThree.GetComponent<SpriteRenderer>().sprite = emptyHeart;
+				break;
+		}
+	}
+}
