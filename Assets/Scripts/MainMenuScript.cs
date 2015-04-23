ec994261a37baff5451586e97063725dcce687d8
Seongmin Choi <sungpa1919@gmail.com>
2015-04-23 09:00:45 -0600

    Pause Menu update

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUITest : MonoBehaviour {
+	
+	void OnGUI () {
+		// Make a background box
+		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+		
+
+		if(GUI.Button(new Rect(20,40,80,20), "Play")) {
+			Application.LoadLevel(1);
+		}
+		
+
+		if(GUI.Button(new Rect(20,70,80,20), "Exit")) {
+			Application.Quit();
+		}
+	}
+}
